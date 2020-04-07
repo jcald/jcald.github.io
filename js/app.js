8b353de90898edd8e8b3a69be384c15b4df5d54b
Jose Calderon-Celis <josecalderoncelis@gmail.com>
2020-04-07 18:09:32 -0500

    sw complete

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ console.log('app.js');
       navigator.serviceWorker.register('sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        console.log('ServiceWorker registration successful with scope: ', registration);
       }, function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
