161519134da58f1822def86b6891a0ff7b7f1513
Jose Calderon-Celis <josecalderoncelis@gmail.com>
2020-04-07 16:47:41 -0500

    Inicio

---
+++ js/app.js
@@ -0,0 +1,29 @@
+console.log('app.js');
+
+
+// // Detectar si podemos usar Service Workers
+// if ( navigator.serviceWorker ) {
+//     navigator.serviceWorker.register('/pwa-basica/sw.js')
+//         .then( reg => { // registration
+//             // console.log( registration );
+
+//             // Suponer posteo offline
+//             setTimeout( () => {
+//                 reg.sync.register('posteo-gatitos');
+//                 console.log('Se enviaron fotos de gatitos al server!');
+//             }, 3000 );
+
+//             // Notification.requestPermission().then( result => {
+//             //     console.log(result);
+//             //     reg.showNotification('Genial!');
+//             // });
+
+//         });
+// }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js');
+    });
+  }
+
---
+++ sw.js
@@ -0,0 +1,90 @@
+console.log("sw.js");
+
+self.addEventListener("fetch", (event) => {
+  console.log("En fetch");
+  if (event.request.url.includes(".jpg")) {
+    console.log(event.request.url);
+    event.respondWith(null);
+    console.log('despues 1er respondWidth')
+    let fotoReq = fetch('img/playa.jpg');
+    event.respondWith(fotoReq);
+    console.log('despues 2do respondWidth')
+  }
+});
+
+// addEventListener('fetch', event => {
+//     // Prevent the default, and handle the request ourselves.
+//     event.respondWith(async function() {
+//       // Intenta obtener la respuesta de un caché.
+//       const cachedResponse = await caches.match(event.request);
+//       // Devuélvelo si encontramos uno.
+//       if (cachedResponse) return cachedResponse;
+//       // Si no encontramos una coincidencia en el caché, use la red.
+//       return fetch(event.request);
+//     }());
+//   });
+
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.url.includes("style.css")) {
+//       // con esto se anula la carga de styles.css
+//     event.respondWith(null);
+//   } else {
+//     event.respondWith(fetch(event.request));
+//   }
+// });
+// =================================== lo de arriba OK
+
+// // Ciclo de vida del SW
+
+// // Instalación del service worker
+// self.addEventListener('install', event => {
+
+//     // Usualmente se almacena en cache
+//     // los recursos de nuestra aplicación
+//     // console.log('SW: Instalando....');
+//     // console.log(event);
+
+//     const instalacion = new Promise( (resolve, reject) => {
+
+//         console.log('SW: Empezando instalación');
+
+//         setTimeout( ()=> {
+//             console.log('SW: Proceso de instalación terminado!!!');
+
+//             // Podemos saltar la espera con este comando
+//             self.skipWaiting();
+//             resolve();
+//         }, 10 ); //1500
+
+//     });
+
+//     event.waitUntil( instalacion );
+
+// });
+
+// // Cuando el service worker toma el control de la aplicación
+// self.addEventListener('activate', event => {
+
+//     console.log('SW: Activo y listo para controlar la aplicación');
+
+// });
+
+// // FETCH: manejo de peticiones http
+// self.addEventListener('fetch', event => {
+
+//     // console.log('Realizando petición a:', event.request.url);
+
+// });
+
+// // SYNC: Recuperación de conexión a internet
+// self.addEventListener( 'sync', event => {
+//     console.log('Tenemos conexión!!!');
+//     console.log(event.tag);
+// });
+
+// // Notificaciones PUSH
+// self.addEventListener('push', event => {
+
+//     console.log('Notificación recibida!!!');
+
+// });
