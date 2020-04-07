8c558f69a5888d41cd1adb802bfb0debaea8aec4
Jose Calderon-Celis <josecalderoncelis@gmail.com>
2020-04-07 18:05:51 -0500

    service work completo

--- js/app.js
+++ js/app.js
@@ -21,9 +21,21 @@ console.log('app.js');
 //         });
 // }
 
-if ('serviceWorker' in navigator) {
+// serviceWorker simple
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//       navigator.serviceWorker.register('sw.js');
+//     });
+//   }
+
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('sw.js');
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
     });
   }
-
