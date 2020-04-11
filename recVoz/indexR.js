40b52a66fe03baa61995439d034ee50619416618
Jose Calderon-Celis <josecalderoncelis@gmail.com>
2020-04-11 00:32:36 -0500

    Reconocimiento de voz 02

---
+++ recVoz/indexR.js
@@ -0,0 +1,27 @@
+console.log('RecVoz: index.js');
+const btnStartRecord = document.getElementById('btnStartRecord');
+const btnStopRecord = document.getElementById('btnStopRecord');
+const texto = document.getElementById('texto');
+
+let recognition = new webkitSpeechRecognition();
+recognition.lang = 'es-ES';
+recognition.continuous = true;
+recognition.interimResults = false;
+console.log('Recognition: ', recognition);
+
+recognition.onresult = (event) => {
+    const results = event.results;
+    console.log(results);
+    const frase = results[results.length - 1][0].transcript;
+    texto.value += frase;
+}
+
+btnStartRecord.addEventListener('click', () => {
+    console.log(btnStartRecord);
+    recognition.start();
+});
+
+btnStopRecord.addEventListener('click', () => {
+    console.log(btnStartRecord);
+    recognition.abort();
+});
